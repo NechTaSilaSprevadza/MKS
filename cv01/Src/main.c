627f51c82c9b5e0bd6ef8250702bebae1c40cc1b
NechTaSilaSprevadza <jozefmak123456@gmail.com>
2021-09-23 12:43:04 +0200

    new stuff

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -26,20 +26,39 @@
 
 int main(void)
 {
-	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA -> MODER |= GPIO_MODER_MODER5_0;
-	GPIOA -> BSRR = (1<<5); // set
-	GPIOA -> BRR = (1<<5); // reset
-	GPIOA -> ODR ^= (1<<5); // toggle
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	GPIOA->BSRR = (1<<5); // set
+	GPIOA->BRR = (1<<5); // reset
+	GPIOA->ODR ^= (1<<5); // toggle
     /* Loop forever */
+/*
 	for(;;)
 	{
 		uint8_t pole[32]={1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0}; // morse code
 		for (uint8_t iter = 0; iter < 32; iter++) // iteration counter
 		{
 			uint8_t morse = pole[iter]; // blink morse code position
-			if (morse == 1) {GPIOA -> BSRR = (1<<5);}
-			else GPIOA -> BRR = (1<<5);
+			if (morse == 1) GPIOA->BSRR = (1<<5);
+			else GPIOA->BRR = (1<<5);
+			for (volatile uint32_t i = 0; i < 100000; i++) {} // time 100ms
+		}
+	}
+*/
+	for(;;)
+	{
+		uint32_t morse32 = 0b10101001110111011100101010000000;
+		for (uint8_t i=0;i<32;i++)
+		{
+			if (morse32 & (1<<31))
+			{
+				GPIOA->BSRR = (1<<5);
+			}
+			else
+			{
+				GPIOA->BRR = (1<<5);
+			}
+			morse32 = morse32<<1;
 			for (volatile uint32_t i = 0; i < 100000; i++) {} // time 100ms
 		}
 	}
