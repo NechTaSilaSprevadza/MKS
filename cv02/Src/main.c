7a1e28fdd4e04229b065158828c7f794a9530dd3
NechTaSilaSprevadza <jozefmak123456@gmail.com>
2021-09-30 11:38:56 +0200

    new

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -19,21 +19,69 @@
 
 #include <stdint.h>
 #include <stm32f0xx.h>
-
+#define LED_TIME_BLINK 300
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
+#define BUTTON_SAMPLE 40
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 void EXTI0_1_IRQHandler(void)
- {
- if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
- EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
- GPIOB->ODR ^= (1<<0); // toggle
- }
- }
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1<<0); // toggle
+	}
+}
+
+volatile uint32_t Tick;
+void SysTick_Handler(void)
+{
+	Tick++; // perioda 1ms
+}
+
+void blikac(void)
+{
+	static uint32_t delay;
+
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1<<4);
+		delay = Tick;
+	}
+}
+
+void tlacitka(void){
+	static uint32_t off_time;
+	static uint32_t delay;
+
+	if (Tick > delay + BUTTON_SAMPLE){
+		static uint32_t old_s2;
+		uint32_t new_s2 = GPIOC->IDR & (1<<0);
+		if (old_s2 && !new_s2) { // falling edge
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s2 = new_s2;
+
+		static uint32_t old_s1;
+		uint32_t new_s1 = GPIOC->IDR & (1<<1);
+		if (old_s1 && !new_s1) { // falling edge
+			off_time = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s1 = new_s1;
+		delay = Tick;
+	}
+
+	if (Tick > off_time) {
+		GPIOB->BRR = (1<<0);
+	}
+}
 
 int main(void)
 {
+	SysTick_Config(8000); // 1ms
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
 	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
@@ -44,4 +92,13 @@ int main(void)
 	EXTI->IMR |= EXTI_IMR_MR0; // mask
 	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
 	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+
+	while (1){
+		blikac();
+		tlacitka();
+	}
 }
+
+
+
+
